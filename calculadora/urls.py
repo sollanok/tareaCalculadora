92ca4b399d82b04cec2d32fb78c3c8889d3d22b7
sollanok <38257870+sollanok@users.noreply.github.com>
2023-03-02 10:29:37 -0600

    oopsies

--- calculadora/urls.py
+++ calculadora/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
 
-    path('proceso', views.proceso, name='proceso'),
+    path('proceso', views.proceso, name = 'proceso'),
 
     # Creación de calculadora de fracciones
-    path('suma', views.suma, name = 'suma'),
-    path('resta', views.resta, name = 'resta'),
-    path('multiplicacion', views.multiplicacion, name = 'multiplicación'),
-    path('division', views.division, name = 'división'),
+    # path('suma', views.suma, name = 'suma'),
+    # path('resta', views.resta, name = 'resta'),
+    # path('multiplicacion', views.multiplicacion, name = 'multiplicación'),
+    # path('division', views.division, name = 'división'),
 ]
--- calculadora/views.py
+++ calculadora/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import tareaCalculadora
 # excentar la necesidad de un token con POST
 from django.views.decorators.csrf import csrf_exempt
 # importar json
@@ -25,86 +24,86 @@ def proceso(request):
     return render(request, 'proceso.html',{'name':nombre})
 
 # Creación de calculadora de fracciones
-@csrf_exempt
-def suma(request):
-    body_unicode = request.body.decode('utf-8')
-    body = loads(body_unicode)
-    numerador1 = body['numerador1']
-    denominador1 = body['denominador1']
-    numerador2 = body['numerador2']
-    denominador2 = body['denominador2']
+#@csrf_exempt
+#def suma(request):
+#    body_unicode = request.body.decode('utf-8')
+#    body = loads(body_unicode)
+#    numerador1 = body['numerador1']
+#    denominador1 = body['denominador1']
+#    numerador2 = body['numerador2']
+#    denominador2 = body['denominador2']
 
     # Proceso de suma
     # Denominadores iguales
-    if denominador1 == denominador2:
-        resultadoNum = numerador1 + numerador2
-        resultado = Fraccion(resultadoNum, denominador1)
+#    if denominador1 == denominador2:
+#        resultadoNum = numerador1 + numerador2
+#        resultado = Fraccion(resultadoNum, denominador1)
     # Denominadores diferentes
-    else:
-        resultadoDen = denominador1 * denominador2
-        resultadoNum = (resultadoDen * numerador1 / denominador1) + (resultadoDen * numerador2 / denominador2)
-        resultado = Fraccion(int(resultadoNum), resultadoDen)
+#    else:
+#        resultadoDen = denominador1 * denominador2
+#        resultadoNum = (resultadoDen * numerador1 / denominador1) + (resultadoDen * numerador2 / denominador2)
+#        resultado = Fraccion(int(resultadoNum), resultadoDen)
 
-    json_resultado = resultado.toJSON()
-    return HttpResponse(json_resultado, \
-                        content_type = "text/json-comment-filtered")
+#    json_resultado = resultado.toJSON()
+#    return HttpResponse(json_resultado, \
+#                        content_type = "text/json-comment-filtered")
 
-@csrf_exempt
-def resta(request):
-    body_unicode = request.body.decode('utf-8')
-    body = loads(body_unicode)
-    numerador1 = body['numerador1']
-    denominador1 = body['denominador1']
-    numerador2 = body['numerador2']
-    denominador2 = body['denominador2']
+#@csrf_exempt
+#def resta(request):
+ #   body_unicode = request.body.decode('utf-8')
+ #   body = loads(body_unicode)
+ #   numerador1 = body['numerador1']
+ #   denominador1 = body['denominador1']
+ #   numerador2 = body['numerador2']
+ #   denominador2 = body['denominador2']
 
     # Proceso de resta
     # Denominadores iguales
-    if denominador1 == denominador2:
-        resultadoNum = numerador1 - numerador2
-        resultado = Fraccion(resultadoNum, denominador1)
+#    if denominador1 == denominador2:
+#        resultadoNum = numerador1 - numerador2
+#        resultado = Fraccion(resultadoNum, denominador1)
     # Denominadores diferentes
-    else:
-        resultadoDen = denominador1 * denominador2
-        resultadoNum = (resultadoDen * numerador1 / denominador1) - (resultadoDen * numerador2 / denominador2)
-        resultado = Fraccion(int(resultadoNum), resultadoDen)
+#    else:
+#        resultadoDen = denominador1 * denominador2
+#        resultadoNum = (resultadoDen * numerador1 / denominador1) - (resultadoDen * numerador2 / denominador2)
+#        resultado = Fraccion(int(resultadoNum), resultadoDen)
 
-    json_resultado = resultado.toJSON()
-    return HttpResponse(json_resultado, \
-                        content_type = "text/json-comment-filtered")
+#    json_resultado = resultado.toJSON()
+#    return HttpResponse(json_resultado, \
+#                        content_type = "text/json-comment-filtered")
 
-@csrf_exempt
-def multiplicacion(request):
-    body_unicode = request.body.decode('utf-8')
-    body = loads(body_unicode)
-    numerador1 = body['numerador1']
-    denominador1 = body['denominador1']
-    numerador2 = body['numerador2']
-    denominador2 = body['denominador2']
+#@csrf_exempt
+#def multiplicacion(request):
+#    body_unicode = request.body.decode('utf-8')
+#    body = loads(body_unicode)
+#    numerador1 = body['numerador1']
+#    denominador1 = body['denominador1']
+#    numerador2 = body['numerador2']
+#    denominador2 = body['denominador2']
 
     # Proceso de multiplicación
-    resultadoNum = numerador1 * numerador2
-    resultadoDen = denominador1 * denominador2
-    resultado = Fraccion(resultadoNum, resultadoDen)
+#    resultadoNum = numerador1 * numerador2
+#    resultadoDen = denominador1 * denominador2
+#    resultado = Fraccion(resultadoNum, resultadoDen)
 
-    json_resultado = resultado.toJSON()
-    return HttpResponse(json_resultado, \
-                        content_type = "text/json-comment-filtered")
+#    json_resultado = resultado.toJSON()
+#    return HttpResponse(json_resultado, \
+#                        content_type = "text/json-comment-filtered")
 
-@csrf_exempt
-def division(request):
-    body_unicode = request.body.decode('utf-8')
-    body = loads(body_unicode)
-    numerador1 = body['numerador1']
-    denominador1 = body['denominador1']
-    numerador2 = body['numerador2']
-    denominador2 = body['denominador2']
+#@csrf_exempt
+#def division(request):
+#    body_unicode = request.body.decode('utf-8')
+#    body = loads(body_unicode)
+#    numerador1 = body['numerador1']
+#    denominador1 = body['denominador1']
+#    numerador2 = body['numerador2']
+#    denominador2 = body['denominador2']
 
     # Proceso de división
-    resultadoNum = numerador1 * denominador2
-    resultadoDen = denominador1 * numerador1
-    resultado = Fraccion(resultadoNum, resultadoDen)
+#    resultadoNum = numerador1 * denominador2
+#    resultadoDen = denominador1 * numerador1
+#    resultado = Fraccion(resultadoNum, resultadoDen)
 
-    json_resultado = resultado.toJSON()
-    return HttpResponse(json_resultado, \
-                        content_type = "text/json-comment-filtered")
+#    json_resultado = resultado.toJSON()
+#    return HttpResponse(json_resultado, \
+#                        content_type = "text/json-comment-filtered")
